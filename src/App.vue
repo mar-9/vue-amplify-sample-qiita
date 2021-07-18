c746207a5c89f505ad3dc04b7699aff8fd6647bc
MASANORI SONE <>
2021-07-18 22:19:27 +0900

    initial commit.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="isSignedIn">
+    <amplify-greetings  :username="this.$store.state.user.username"></amplify-greetings>
+    <router-link custom v-slot="{ navigate }" :to="{ name: 'AlbumIndex' }">
+      <button @click="navigate">TOP</button>
+    </router-link>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  computed: {
+    isSignedIn() {
+      return this.$store.state.user !== null;
+    },
+  },
+};
+</script>
