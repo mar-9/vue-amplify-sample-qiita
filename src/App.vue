e643e1eddd0f08abb6a49d10380f569591ee2efa
MASANORI SONE <>
2021-08-30 21:25:54 +0900

    add quasar.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,116 @@
-<template>
+<template>  
   <div v-if="isSignedIn">
     <amplify-greetings  :username="this.$store.state.user.username"></amplify-greetings>
     <router-link custom v-slot="{ navigate }" :to="{ name: 'AlbumIndex' }">
-      <button @click="navigate">TOP</button>
+      <q-btn color="primary" @click="navigate" label="TOP"/>
     </router-link>
   </div>
-  <router-view/>
+
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          My Album
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+        <q-btn outline style="color: white;" @click="Auth.signOut();" label="Sign Out"/>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label caption>chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="forum" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Forum</q-item-label>
+            <q-item-label caption>forum.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Auth from 'aws-amplify'
+
 export default {
   computed: {
     isSignedIn() {
       return this.$store.state.user !== null;
     },
   },
-};
+
+  name: 'LayoutDefault',
+
+  components: {
+    HelloWorld
+  },
+
+  setup () {
+    return {
+      leftDrawerOpen: ref(false)
+    }
+  }
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from './router'
 import store from './store'
 import Amplify from 'aws-amplify'
 import aws_exports from './aws-exports'
-import {
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+import {  
   applyPolyfills,
   defineCustomElements
 } from '@aws-amplify/ui-components/loader'
@@ -13,5 +15,19 @@ Amplify.configure(aws_exports)
 applyPolyfills().then(() => {
   defineCustomElements(window)
 })
-
-createApp(App).use(store).use(router).mount('#app')
+let languageDict = {
+    ja:{
+        'Sign in to your account' : 'アカウントにサインイン',
+        'Username *' : 'ユーザ名 *',
+        'Enter your username' : 'ユーザ名を入力してください',
+        'Password *' : 'パスワード *',
+        'Enter your password' : 'パスワードを入力してください',
+        'Forgot your password?' : 'パスワードを忘れた場合',
+        'Reset password' : 'パスワードのリセット',
+        'No account?' : 'まだアカウントがない場合',
+        'Create account' : 'アカウント作成',
+        'Sign In' : 'サインイン',
+    }
+}
+Amplify.I18n.putVocabularies(languageDict)
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
